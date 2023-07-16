04ceae3106d7fedf2d38c1a27554a6333abc447a
anushya <55213485+anushyashankar@users.noreply.github.com>
2023-07-16 10:22:17 -0400

    Update CircularSinglyLinkedList.java

--- CircularSinglyLinkedList.java
+++ CircularSinglyLinkedList.java
@@ -1,38 +1,10 @@
 import java.util.NoSuchElementException;
-/**
- * Your implementation of a CircularSinglyLinkedList without a tail pointer.
- *
- * @author Anushya Shankar
- * @version 1.0
- * @userid ashankar92
- * @GTID 903826158
- *
- * Collaborators: n/a
- *
- * Resources: n/a
- */
+
 public class CircularSinglyLinkedList<T> {
 
-    /*
-     * Do not add new instance variables or modify existing ones.
-     */
     private CircularSinglyLinkedListNode<T> head;
     private int size;
 
-    /*
-     * Do not add a constructor.
-     */
-
-    /**
-     * Adds the data to the specified index.
-     *
-     * Must be O(1) for indices 0 and size and O(n) for all other cases.
-     *
-     * @param index the index at which to add the new data
-     * @param data  the data to add at the specified index
-     * @throws java.lang.IndexOutOfBoundsException if index < 0 or index > size
-     * @throws java.lang.IllegalArgumentException  if data is null
-     */
     public void addAtIndex(int index, T data) {
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("Index is not in the bounds of this list.");
@@ -57,14 +29,6 @@ public class CircularSinglyLinkedList<T> {
         }
     }
 
-    /**
-     * Adds the data to the front of the list.
-     *
-     * Must be O(1).
-     *
-     * @param data the data to add to the front of the list
-     * @throws java.lang.IllegalArgumentException if data is null
-     */
     public void addToFront(T data) {
         if (data == null) {
             throw new IllegalArgumentException("The data entered is invalid.");
@@ -81,14 +45,6 @@ public class CircularSinglyLinkedList<T> {
         size++;
     }
 
-    /**
-     * Adds the data to the back of the list.
-     *
-     * Must be O(1).
-     *
-     * @param data the data to add to the back of the list
-     * @throws java.lang.IllegalArgumentException if data is null
-     */
     public void addToBack(T data) {
         if (data == null) {
             throw new IllegalArgumentException("The data entered is invalid.");
@@ -106,15 +62,6 @@ public class CircularSinglyLinkedList<T> {
         size++;
     }
 
-    /**
-     * Removes and returns the data at the specified index.
-     *
-     * Must be O(1) for index 0 and O(n) for all other cases.
-     *
-     * @param index the index of the data to remove
-     * @return the data formerly located at the specified index
-     * @throws java.lang.IndexOutOfBoundsException if index < 0 or index >= size
-     */
     public T removeAtIndex(int index) {
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("Index is not in the bounds of this list.");
@@ -137,14 +84,6 @@ public class CircularSinglyLinkedList<T> {
         }
     }
 
-    /**
-     * Removes and returns the first data of the list.
-     *
-     * Must be O(1).
-     *
-     * @return the data formerly located at the front of the list
-     * @throws java.util.NoSuchElementException if the list is empty
-     */
     public T removeFromFront() {
         if (isEmpty()) {
             throw new NoSuchElementException("List is empty, nothing to remove.");
@@ -162,14 +101,6 @@ public class CircularSinglyLinkedList<T> {
         return temp;
     }
 
-    /**
-     * Removes and returns the last data of the list.
-     *
-     * Must be O(n).
-     *
-     * @return the data formerly located at the back of the list
-     * @throws java.util.NoSuchElementException if the list is empty
-     */
     public T removeFromBack() {
         if (isEmpty()) {
             throw new NoSuchElementException("List is empty, nothing to remove.");
@@ -178,15 +109,6 @@ public class CircularSinglyLinkedList<T> {
         return removeAtIndex(size - 1);
     }
 
-    /**
-     * Returns the data at the specified index.
-     *
-     * Should be O(1) for index 0 and O(n) for all other cases.
-     *
-     * @param index the index of the data to get
-     * @return the data stored at the index in the list
-     * @throws java.lang.IndexOutOfBoundsException if index < 0 or index >= size
-     */
     public T get(int index) {
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("Index is not in the bounds of this list.");
@@ -201,42 +123,15 @@ public class CircularSinglyLinkedList<T> {
         return (T) curr.getData();
     }
 
-    /**
-     * Returns whether or not the list is empty.
-     *
-     * Must be O(1).
-     *
-     * @return true if empty, false otherwise
-     */
     public boolean isEmpty() {
         return (size == 0);
     }
 
-    /**
-     * Clears the list.
-     *
-     * Clears all data and resets the size.
-     *
-     * Must be O(1).
-     */
     public void clear() {
         head = null;
         size = 0;
     }
 
-    /**
-     * Removes and returns the last copy of the given data from the list.
-     *
-     * Do not return the same data that was passed in. Return the data that
-     * was stored in the list.
-     *
-     * Must be O(n).
-     *
-     * @param data the data to be removed from the list
-     * @return the data that was removed
-     * @throws java.lang.IllegalArgumentException if data is null
-     * @throws java.util.NoSuchElementException   if data is not found
-     */
     public T removeLastOccurrence(T data) {
         if (data == null) {
             throw new IllegalArgumentException("The data entered is invalid.");
@@ -269,14 +164,6 @@ public class CircularSinglyLinkedList<T> {
         return remove;
     }
 
-    /**
-     * Returns an array representation of the linked list.
-     *
-     * Must be O(n) for all cases.
-     *
-     * @return the array of length size holding all of the data (not the
-     * nodes) in the list in the same order
-     */
     public T[] toArray() {
         T[] arr = (T[]) new Object[size];
         CircularSinglyLinkedListNode<T> curr = head;
@@ -289,29 +176,11 @@ public class CircularSinglyLinkedList<T> {
         return arr;
     }
 
-    /**
-     * Returns the head node of the list.
-     *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
-     * @return the node at the head of the list
-     */
     public CircularSinglyLinkedListNode<T> getHead() {
-        // DO NOT MODIFY!
         return head;
     }
 
-    /**
-     * Returns the size of the list.
-     *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
-     * @return the size of the list
-     */
     public int size() {
-        // DO NOT MODIFY!
         return size;
     }
 }
