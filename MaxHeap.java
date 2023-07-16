73bdfde5e0ec8b28eb70ed92d2ef2fc92ff6972f
anushya <55213485+anushyashankar@users.noreply.github.com>
2023-07-16 10:27:08 -0400

    Update MaxHeap.java

--- MaxHeap.java
+++ MaxHeap.java
@@ -1,68 +1,17 @@
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
-/**
- * Your implementation of a MaxHeap.
- *
- * @author Anushya Shankar
- * @version 1.0
- * @userid ashankar92
- * @GTID 903826158
- *
- * Collaborators: n/a
- *
- * Resources: n/a
- */
 public class MaxHeap<T extends Comparable<? super T>> {
 
-    /*
-     * The initial capacity of the MaxHeap when created with the default
-     * constructor.
-     *
-     * DO NOT MODIFY THIS VARIABLE!
-     */
     public static final int INITIAL_CAPACITY = 13;
 
-    /*
-     * Do not add new instance variables or modify existing ones.
-     */
     private T[] backingArray;
     private int size;
 
-    /**
-     * Constructs a new MaxHeap.
-     *
-     * The backing array should have an initial capacity of INITIAL_CAPACITY.
-     */
     public MaxHeap() {
         backingArray = (T[]) new Comparable[INITIAL_CAPACITY];
     }
 
-    /**
-     * Creates a properly ordered heap from a set of initial values.
-     *
-     * You must use the BuildHeap algorithm that was taught in lecture! Simply
-     * adding the data one by one using the add method will not get any credit.
-     * As a reminder, this is the algorithm that involves building the heap
-     * from the bottom up by repeated use of downHeap operations.
-     *
-     * Before doing the algorithm, first copy over the data from the
-     * ArrayList to the backingArray (leaving index 0 of the backingArray
-     * empty). The data in the backingArray should be in the same order as it
-     * appears in the passed in ArrayList before you start the BuildHeap
-     * algorithm.
-     *
-     * The backingArray should have capacity 2n + 1 where n is the
-     * number of data in the passed in ArrayList (not INITIAL_CAPACITY).
-     * Index 0 should remain empty, indices 1 to n should contain the data in
-     * proper order, and the rest of the indices should be empty.
-     *
-     * Consider how to most efficiently determine if the list contains null data.
-     * 
-     * @param data a list of data to initialize the heap with
-     * @throws java.lang.IllegalArgumentException if data or any element in data
-     *                                            is null
-     */
     public MaxHeap(ArrayList<T> data) {
         if (data == null) {
             throw new IllegalArgumentException("ArrayList passed in was null.");
@@ -83,16 +32,6 @@ public class MaxHeap<T extends Comparable<? super T>> {
         buildHeap(size / 2);
     }
 
-    /**
-     * Adds the data to the heap.
-     *
-     * If sufficient space is not available in the backing array (the backing
-     * array is full except for index 0), resize it to double the current
-     * length.
-     *
-     * @param data the data to add
-     * @throws java.lang.IllegalArgumentException if data is null
-     */
     public void add(T data) {
         if (data == null) {
             throw new IllegalArgumentException("Data entered is null.");
@@ -112,16 +51,6 @@ public class MaxHeap<T extends Comparable<? super T>> {
         upHeap(size);
     }
 
-    /**
-     * Removes and returns the root of the heap.
-     *
-     * Do not shrink the backing array.
-     *
-     * Replace any unused spots in the array with null.
-     *
-     * @return the data that was removed
-     * @throws java.util.NoSuchElementException if the heap is empty
-     */
     public T remove() {
         if (isEmpty()) {
             throw new NoSuchElementException("Heap is empty, nothing to remove.");
@@ -137,12 +66,6 @@ public class MaxHeap<T extends Comparable<? super T>> {
         return removed;
     }
 
-    /**
-     * Returns the maximum element in the heap.
-     *
-     * @return the maximum element
-     * @throws java.util.NoSuchElementException if the heap is empty
-     */
     public T getMax() {
         if (isEmpty()) {
             throw new NoSuchElementException("Heap is empty, nothing to return.");
@@ -150,67 +73,30 @@ public class MaxHeap<T extends Comparable<? super T>> {
         return backingArray[1];
     }
 
-    /**
-     * Returns whether or not the heap is empty.
-     *
-     * @return true if empty, false otherwise
-     */
     public boolean isEmpty() {
         return size == 0;
     }
 
-    /**
-     * Clears the heap.
-     *
-     * Resets the backing array to a new array of the initial capacity and
-     * resets the size.
-     */
     public void clear() {
         backingArray = (T[]) new Comparable[INITIAL_CAPACITY];
         size = 0;
     }
 
-    /**
-     * Returns the backing array of the heap.
-     *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
-     * @return the backing array of the list
-     */
     public T[] getBackingArray() {
-        // DO NOT MODIFY THIS METHOD!
         return backingArray;
     }
 
-    /**
-     * Returns the size of the heap.
-     *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
-     * @return the size of the list
-     */
     public int size() {
-        // DO NOT MODIFY THIS METHOD!
         return size;
     }
 
-    /**
-     * Recursive function to build a heap.
-     * @param index index to build heap at
-     */
     private void buildHeap(int index) {
         if (index != 0) {
             downHeap(index);
             buildHeap(--index);
         }
     }
-
-    /**
-     * Recursive function to downheap.
-     * @param index index to downheap at
-     */
+    
     private void downHeap(int index) {
         if (2 * index <= size && backingArray[index].compareTo(backingArray[2 * index]) < 0
                 || 2 * index + 1 <= size && backingArray[index].compareTo(backingArray[index * 2 + 1]) < 0) {
@@ -228,10 +114,6 @@ public class MaxHeap<T extends Comparable<? super T>> {
         }
     }
 
-    /**
-     * Upheap at a given index.
-     * @param index index to upheap at
-     */
     private void upHeap(int index) {
         if (index != 1 && backingArray[index].compareTo(backingArray[index / 2]) > 0) {
             T temp = backingArray[index];
