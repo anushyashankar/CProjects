befdc04c2aec740863b840c5e2a8ca3788f262f9
anushya <55213485+anushyashankar@users.noreply.github.com>
2023-07-16 10:25:24 -0400

    Update LinkedQueue.java

--- LinkedQueue.java
+++ LinkedQueue.java
@@ -1,38 +1,11 @@
 import java.util.NoSuchElementException;
 
-/**
- * Your implementation of a LinkedQueue. It should NOT be circular.
- *
- * @author Anushya Shankar
- * @version 1.0
- * @userid ashankar92
- * @GTID 903826158
- *
- * Collaborators: n/a
- *
- * Resources: n/a
- */
 public class LinkedQueue<T> {
 
-    /*
-     * Do not add new instance variables or modify existing ones.
-     */
     private LinkedNode<T> head;
     private LinkedNode<T> tail;
     private int size;
 
-    /*
-     * Do not add a constructor.
-     */
-
-    /**
-     * Adds the data to the back of the queue.
-     *
-     * Must be O(1).
-     *
-     * @param data the data to add to the back of the queue
-     * @throws java.lang.IllegalArgumentException if data is null
-     */
     public void enqueue(T data) {
         if (data == null) {
             throw new IllegalArgumentException("The data entered is null.");
@@ -48,14 +21,6 @@ public class LinkedQueue<T> {
         size++;
     }
 
-    /**
-     * Removes and returns the data from the front of the queue.
-     *
-     * Must be O(1).
-     *
-     * @return the data formerly located at the front of the queue
-     * @throws java.util.NoSuchElementException if the queue is empty
-     */
     public T dequeue() {
         if (size == 0) {
             throw new NoSuchElementException("Queue is empty, nothing to dequeue.");
@@ -66,14 +31,6 @@ public class LinkedQueue<T> {
         return dequeued.getData();
     }
 
-    /**
-     * Returns the data from the front of the queue without removing it.
-     *
-     * Must be O(1).
-     *
-     * @return the data located at the front of the queue
-     * @throws java.util.NoSuchElementException if the queue is empty
-     */
     public T peek() {
         if (size == 0) {
             throw new NoSuchElementException("Queue is empty, nothing to peek at.");
@@ -81,42 +38,15 @@ public class LinkedQueue<T> {
         return head.getData();
     }
 
-    /**
-     * Returns the head node of the queue.
-     *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
-     * @return the node at the head of the queue
-     */
     public LinkedNode<T> getHead() {
-        // DO NOT MODIFY THIS METHOD!
         return head;
     }
 
-    /**
-     * Returns the tail node of the queue.
-     *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
-     * @return the node at the tail of the queue
-     */
     public LinkedNode<T> getTail() {
-        // DO NOT MODIFY THIS METHOD!
         return tail;
     }
 
-    /**
-     * Returns the size of the queue.
-     *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
-     * @return the size of the queue
-     */
     public int size() {
-        // DO NOT MODIFY THIS METHOD!
         return size;
     }
 }
