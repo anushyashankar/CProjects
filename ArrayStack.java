f6cd111347f5aaf33c5b0fa45d9e301464e0930f
anushya <55213485+anushyashankar@users.noreply.github.com>
2023-07-16 10:19:14 -0400

    Update ArrayStack.java

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,51 +1,17 @@
 import java.util.NoSuchElementException;
 
-/**
- * Your implementation of an ArrayStack.
- *
- * @author Anushya Shankar
- * @version 1.0
- * @userid ashankar92
- * @GTID 903826158
- *
- * Collaborators: n/a
- *
- * Resources: n/a
- */
 public class ArrayStack<T> {
 
-    /*
-     * The initial capacity of the ArrayStack.
-     *
-     * DO NOT MODIFY THIS VARIABLE.
-     */
     public static final int INITIAL_CAPACITY = 9;
 
-    /*
-     * Do not add new instance variables or modify existing ones.
-     */
     private T[] backingArray;
     private int size;
 
-    /**
-     * Constructs a new ArrayStack.
-     */
     public ArrayStack() {
         backingArray = (T[]) new Object[INITIAL_CAPACITY];
         size = 0;
     }
 
-    /**
-     * Adds the data to the top of the stack.
-     *
-     * If sufficient space is not available in the backing array, resize it to
-     * double the current length.
-     *
-     * Must be amortized O(1).
-     *
-     * @param data the data to add to the top of the stack
-     * @throws java.lang.IllegalArgumentException if data is null
-     */
     public void push(T data) {
         if (data == null) {
             throw new IllegalArgumentException("The data entered is null.");
@@ -60,19 +26,7 @@ public class ArrayStack<T> {
         backingArray[size] = data;
         size++;
     }
-
-    /**
-     * Removes and returns the data from the top of the stack.
-     *
-     * Do not shrink the backing array.
-     *
-     * Replace any spots that you pop from with null.
-     *
-     * Must be O(1).
-     *
-     * @return the data formerly located at the top of the stack
-     * @throws java.util.NoSuchElementException if the stack is empty
-     */
+    
     public T pop() {
         if (size == 0) {
             throw new NoSuchElementException("The stack is empty, nothing to pop.");
@@ -83,14 +37,6 @@ public class ArrayStack<T> {
         return popped;
     }
 
-    /**
-     * Returns the data from the top of the stack without removing it.
-     *
-     * Must be O(1).
-     *
-     * @return the data from the top of the stack
-     * @throws java.util.NoSuchElementException if the stack is empty
-     */
     public T peek() {
         if (size == 0) {
             throw new NoSuchElementException("The stack is empty, nothing to pop.");
@@ -99,29 +45,11 @@ public class ArrayStack<T> {
         return backingArray[size - 1];
     }
 
-    /**
-     * Returns the backing array of the stack.
-     *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
-     * @return the backing array of the stack
-     */
     public T[] getBackingArray() {
-        // DO NOT MODIFY THIS METHOD!
         return backingArray;
     }
 
-    /**
-     * Returns the size of the stack.
-     *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
-     * @return the size of the stack
-     */
     public int size() {
-        // DO NOT MODIFY THIS METHOD!
         return size;
     }
 }
